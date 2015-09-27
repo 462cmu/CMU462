4671344fc628410e4fd47831813d9b2b67d2889c
Michael Choquette <mchoquet1@gmail.com>
2015-09-26 23:17:03 -0400

    added infinity constants to misc_math

--- include/CMU462/misc_math.h
+++ include/CMU462/misc_math.h
@@ -1,12 +1,15 @@
 #ifndef CMU462_MISCMATH_H
 #define CMU462_MISCMATH_H
 
-#include <utility>
 #include <cmath>
+#include <limits>
+#include <utility>
 
 #define PI (3.14159265358979323)
 #define EPS_D (0.00000000001)
 #define EPS_F (0.00001f)
+#define INF_D (std::numeric_limits<double>::infinity())
+#define INF_F (std::numeric_limits<float>::infinity())
 
 namespace CMU462 {
 
